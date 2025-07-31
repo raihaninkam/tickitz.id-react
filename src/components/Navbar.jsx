429320e41a5ee12aa03c474ca4fea09894ae5131
Raihaninkam <raihankamil37@gmail.com>
2025-07-31 09:09:08 +0700

    fix: switch navigation from anchor tag to Link tag & URL image path hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,56 @@
+import {Link} from "react-router";
+
 const MyNavbar = () => {
-    return (
+  return (
     <>
-        <header className="bg-white border-b border-gray-200">
-      <nav className="flex justify-between items-center px-8 lg:px-20 py-4">
-        <div className="flex items-center">
-          <img
-            src="tickitz 1.svg"
-            alt="Tickitz Logo"
-            className="h-8"
-          />
-        </div>
+      <header className="bg-white border-b border-gray-200">
+        <nav className="flex justify-between items-center px-8 lg:px-20 py-4">
+          <div className="flex items-center">
+            <img src="tickitz 1.svg" alt="Tickitz Logo" className="h-8" />
+          </div>
 
-        <div className="hidden md:flex space-x-8">
-          <a href="#" className="text-gray-800 hover:text-primary transition-colors"
-            >Home</a
-          >
-          <a href="#" className="text-gray-800 hover:text-primary transition-colors"
-            >Movie</a
-          >
-          <a href="#" className="text-gray-800 hover:text-primary transition-colors"
-            >Buy Ticket</a
-          >
-        </div>
+          <div className="hidden md:flex space-x-8">
+            <Link to="/home"
+              className="text-gray-800 hover:text-primary transition-colors"
+              >Home
+            </Link>
 
-        <div className="hidden md:flex space-x-4">
-          <a
-            href="#"
-            className="px-6 py-2 border border-primary text-primary rounded-lg hover:bg-primary hover:text-white transition-colors"
-            >Sign In</a
-          >
-          <a
-            href="#"
-            className="px-6 py-2 bg-primary text-white rounded-lg hover:bg-blue-700 transition-colors"
-            >Sign Up</a
-          >
-        </div>
+            <Link
+              to="/movies"
+              className="text-gray-800 hover:text-primary transition-colors"
+            >
+              Movie
+            </Link>
 
-        <div className="md:hidden">
-          <img src="./assets/img/gg_menu-right-alt.svg" alt=""/>
-        </div>
-      </nav>
-    </header>
+            <Link
+              to=""
+              className="text-gray-800 hover:text-primary transition-colors"
+            >
+              Buy Ticket
+            </Link>
+          </div>
 
+          <div className="hidden md:flex space-x-4">
+            <Link to ="/Login"
+              className="px-6 py-2 border text-blue-700 border-blue-700 rounded-lg hover:bg-primary hover:text-white transition-colors"
+            >
+              Sign In
+            </Link>
+            <Link to= "/Register"
+              
+              className="px-6 py-2 bg-blue-700 text-white rounded-lg hover:bg-blue-600 transition-colors"
+            >
+              Sign Up
+            </Link>
+          </div>
+
+          <div className="md:hidden">
+            <img src="gg_menu-right-alt.svg" alt="" />
+          </div>
+        </nav>
+      </header>
     </>
-    );
-}
+  );
+};
 
 export default MyNavbar;
