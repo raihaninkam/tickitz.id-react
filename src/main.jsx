026a7f5d886b057c8af23d6d5659624469573cba
Raihaninkam <raihankamil37@gmail.com>
2025-07-31 09:15:39 +0700

    fix: change url import path App.jsx to Router.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './Router.jsx'
+// import tailwindcss from '@tailwindcss/vite'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
