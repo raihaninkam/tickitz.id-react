ba90ae9de50760f2211fcf884aad1718e13a08a3
Raihaninkam <raihankamil37@gmail.com>
2025-08-04 10:09:17 +0700

    feat: complete Routes for all component

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,52 +1,46 @@
-
 import Login from "./components/Login.jsx";
 import MovieApp from "./pages/Movie.jsx";
-import MyFooter from "./components/Footer.jsx";
-import MyNavbar from "./components/Navbar.jsx";
 import Register from "./components/Register.jsx";
 import Home from "./pages/Home.jsx";
-import PaymentPage from "./pages/payment.jsx";
+import PaymentPage from "./pages/Payment.jsx";
 import OrderPage from "./pages/Order.jsx";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router";
+import MovieDetailPage from "./pages/details.jsx";
+import { BrowserRouter, Routes, Route} from "react-router";
+import TicketResult from "./pages/Ticket.jsx";
+import ProfilePage from "./pages/Profile.jsx";
+import OrderHistory from "./pages/OrderHistory.jsx";
+// import TickitzDashboard from "./pages/Chart.jsx";
+import TickitzMovieCRUD from "./pages/Crud.jsx";
+
 
 function App() {
   return (
     <>
-
-
-    <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          <Route path="/">
-            <Route index element={<MyLayout />} />
-            <Route path="register" element={<Register />} />
-            <Route path="login" element={<Login />} />
-            <Route path="movies" element={<MovieApp />} />
-            <Route path="Home" element={<Home />} />
-            <Route path="payment" element={< PaymentPage/>} />
-            <Route path="order" element={< OrderPage/>} />
+          {/* Auth routes */}
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          
+          {/* Main app routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/home/movies" element={<MovieApp />} />
+          <Route path="/home/movies/:id" element={<MovieDetailPage />} />
+          <Route path="/home/payment" element={<PaymentPage />} />
+          <Route path="/home/order" element={<OrderPage />} />
+          <Route path="/home/ticket" element={<TicketResult/>} />
 
-          </Route>
+          {/* dashboard app routes */}
+          <Route path="/" element={<ProfilePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/order-history" element={<OrderHistory />} />
+          {/* <Route path="/dashboard" element={<TickitzDashboard />} /> */}
+          <Route path="/crud" element={<TickitzMovieCRUD />} />
         </Routes>
       </BrowserRouter>
-
-    
-
-
-
-
     </>
   );
-}
-
-
-function MyLayout () {
-  return(
-    <>
-      <MyNavbar/>
-      <Outlet/>
-      <MyFooter/>
-    </>
-  )
 }
 
 export default App;
